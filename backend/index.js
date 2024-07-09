e84464efe4baae406c7822646cdfb12c84f787c1
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-07-09 13:11:46 +0100

    index.ejs updated

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const PORT=3000;
+
+const app = express();
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("../../frontend/views/index.ejs")
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
