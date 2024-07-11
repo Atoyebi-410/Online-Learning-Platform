4095212df3849e64a9802fe2c55180592d6becd0
Ibrahim Olanrewaju <olanrewajubrahim22@gmail.com>
2024-07-11 14:57:02 +0100

    debugging

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const { User, Course } = require("../models")
+const { User } = require("../models")
 
 const router = express.Router();
 
 // create a new user
-router.post("/register", async (req, res) => {
+router.get("/register", async (req, res) => {
     try {
-        
         const { firstName, lastName, email, password } = req.body;
 
         // check for missing fields
@@ -20,7 +19,7 @@ router.post("/register", async (req, res) => {
     } catch (error) {
         res.status(400).json({error: error.message})
     }
-});
+})
 
 // get all the existing users
 router.get("/users", async (req, res) => {
@@ -33,25 +32,4 @@ router.get("/users", async (req, res) => {
     }
 })
 
-// create a new course
-router.post("/course", async (req, res) => {
-    try {
-        const { title, description, userId } = req.body;
-        const newCourse = await Course.create({ title, description, userId })
-        res.status(201).json(newCourse);
-    } catch (error) {
-        res.status(400).json({error: error.message})
-    }
-});
-
-router.get("/courses", async (req, res) => {
-    try {
-        const courses = await Course.findAll();
-        res.status(200).json(courses);
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-        
-    }
-})
-
-module.exports = router;
+module.exports = router
